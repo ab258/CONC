6c35ef67b9155bca37fcbc6e1f35450fbd421771
bhusarik <123774184+bhusarik@users.noreply.github.com>
2023-01-27 12:50:38 -0800

    Create ToolpathToGCode.py

---
+++ ToolpathToGCode.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().run_line_magic('matplotlib', 'notebook')
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
+from scipy.io import wavfile
+import librosa
+import csv
+import pandas as pd
+from collections import Counter
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
+
